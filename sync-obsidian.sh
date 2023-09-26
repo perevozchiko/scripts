0fa72ebbcff7e7f3f20af6bb5cc7426dc18449ec
andrei <andreiperevozchikov@macbook.local>
2023-09-26 09:46:50 +0200

    add sync obsidian script

---
+++ sync-obsidian.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+currentPath=$(pwd)
+obsidianPath=$0
+now=$(date + "%Y-%m-%d-%I-%M-%S")
+cd $obsidianPath 
+git add .
+git commit -am "work-macbook sync obsidian notes $now"
+git pull --rebase
+git push
+git status
+cd $currentPath
